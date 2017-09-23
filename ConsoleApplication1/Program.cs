0564106780d1028cbf834dd86ff02ef4a49f45c3
hillmk <hillmk_01@hotmail.com>
2017-09-23 13:26:34 +1000

    ccc

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApplication1
         {
             for (int i = 1; i<=10;i++)
             {
-                Console.WriteLine(i.ToString() + ".." + "....aabbb");
+                Console.WriteLine(i.ToString() + ".." + "....aabbbccc");
             }
         }
     }
