de0740ee3d7e90be37e12e9f9e71e114dfa7b1c5
hillmk <hillmk_01@hotmail.com>
2017-09-23 13:38:06 +1000

    fix

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -13,8 +13,6 @@ namespace ConsoleApplication1
             for (int i = 1; i<=10;i++)
             {
                 Console.WriteLine(i.ToString() + ".." + "....aabbbcccddd");
-                args;
-
             }
         }
     }
