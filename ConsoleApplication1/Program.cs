a039c828638aa5c75044e2a094620867de0905db
hillmk <hillmk_01@hotmail.com>
2017-09-23 13:12:47 +1000

    added aa

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApplication1
         {
             for (int i = 1; i<=10;i++)
             {
-                Console.WriteLine(i.ToString() + ".." + "....");
+                Console.WriteLine(i.ToString() + ".." + "....aa");
             }
         }
     }
